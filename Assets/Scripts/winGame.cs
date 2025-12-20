8abe7c502819eb3c81d6812988cb858ec0589c5a
Roxas255 <aemartinez1224@icloud.com>
2025-12-19 20:51:49 -0500

    Portal art

--- Assets/Scripts/winGame.cs
+++ Assets/Scripts/winGame.cs
@@ -3,25 +3,41 @@ using UnityEngine.SceneManagement;
 
 public class winGame : MonoBehaviour
 {
+    [Header("Win Scene")]
     [SerializeField] private int winSceneBuildIndex = 3;
+
+    [Header("Trigger")]
+    [SerializeField] private string playerTag = "Player";
+
     private bool hasWon = false;
+
+    private void Reset()
+    {
+        // Auto-make the collider a trigger (nice when attaching to the portal)
+        var col = GetComponent<Collider2D>();
+        if (col != null) col.isTrigger = true;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (hasWon) return;
 
-        // IMPORTANT: make sure we're reacting to the PLAYER ROOT object
+        // React to PLAYER ROOT (works even if player has child colliders)
         var playerRoot = other.attachedRigidbody ? other.attachedRigidbody.gameObject : other.gameObject;
-        if (!playerRoot.CompareTag("Player")) return;
+        if (!playerRoot.CompareTag(playerTag)) return;
 
         hasWon = true;
 
         string levelId = SceneManager.GetActiveScene().name;
 
-        var sw = UnityEngine.Object.FindFirstObjectByType<stopwatchControl>();
+        // Save time if stopwatch exists
+        var sw = Object.FindFirstObjectByType<stopwatchControl>();
         if (sw != null)
+        {
             LevelScoreboard.AddScore(levelId, sw.GetElapsedTime());
+        }
 
+        // Tell Win Scene which level was just completed
         PlayerPrefs.SetString(LevelScoreboard.LastLevelKey, levelId);
         PlayerPrefs.Save();
 
